c8a167ed858ba7402aa871b02caf5e9ddcc15568
seung-heee <tmdgmldi2595@naver.com>
2024-10-24 16:25:26 +0900

    feat: product 테이블생성

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,9 +1,11 @@
 const db = require('../config/db');
+require('dotenv').config();
 
 // 모든 상품 조회
 exports.getAllProducts = async (req, res) => {
   try {
     const [products] = await db.query('SELECT * FROM product');
+
     res.status(200).json({ message: '상품 조회 성공', data: products });
   } catch (error) {
     console.error('상품 조회 오류:', error);
