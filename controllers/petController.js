22fb33a76fab26b6d23750b9eb2d46de2d807316
김예원 <yaewon4643@naver.com>
2024-10-30 17:38:06 +0900

    Feat: 반려동물 조회 및 수정

--- controllers/petController.js
+++ controllers/petController.js
@@ -23,3 +23,27 @@ exports.registerPet = async (req, res) => {
     res.status(500).json({ message: '반려동물 정보 저장 실패' });
   }
 };
+
+// 회원별 반려동물 리스트 조회
+exports.getPet = async (req, res) => {
+  const userId = req.params.id;
+  try {
+    const [pets] = await db.query('SELECT * FROM pet WHERE user_id = ?', [userId]);
+    res.status(200).json(pets);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: "반려동물 조회 실패"})
+  }
+}
+
+// 반려동물별 정보 조회
+exports.getPetDetail = async (req, res) => {
+  const petId = req.params.id;
+  try {
+    const pet = await db.query('SELECT * FROM pet WHERE id=?', [petId]);
+    res.status(200).json(pet[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({message: "반려동물 정보 조회 실패"})
+  }
+}
--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { registerPet } = require('../controllers/petController');
+const { registerPet, getPet, getPetDetail } = require('../controllers/petController');
 
 router.post('/animal-register', registerPet);
+
+// 회원별 반려동물 조회
+router.get('/:id', getPet);
+
+// 반려동물별 정보 조회
+router.get('/detail/:id', getPetDetail);
 
 module.exports = router;
