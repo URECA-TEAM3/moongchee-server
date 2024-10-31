9d8b7d8b5abfd28fcc295b478b5781d5d1f66369
Suhjin Kang <55722299+skang9810@users.noreply.github.com>
2024-10-31 15:45:45 +0900

    Feature / cancel order (#14)
    * get point by id
    
    * updatePoint fix

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -109,10 +109,11 @@ exports.updatePoints = async (req, res) => {
     70000: 750,
     100000: 1100,
   };
+  const value = amount > 1100 ? amountToPoints[amount] : amount;
 
   try {
     const query = `UPDATE member SET point = point + ? where id = ?`;
-    const [rows] = await db.query(query, [amountToPoints[amount], userId]);
+    const [rows] = await db.query(query, [value, userId]);
 
     if (rows.affectedRows === 0) return res.status(404).json({ message: '사용자를 찾을 수 없습니다.' });
 
@@ -120,5 +121,20 @@ exports.updatePoints = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: '서버 오류. 다시 시도해주세요.' });
+  }
+};
+
+exports.getPoint = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const [result] = await db.query('SELECT point FROM member WHERE id = ?', [userId]); // ID로 상품 조회
+
+    if (result.length === 0) return res.status(404).json({ message: '상품을 찾을 수 없습니다.' }); // 상품이 없을 경우
+
+    res.status(200).json({ message: '포인트 조회 성공', data: result[0] });
+  } catch (error) {
+    console.error('포인트 조회 오류:', error);
+    res.status(500).json({ message: '포인트 조회 실패' });
   }
 };
