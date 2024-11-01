61f0449012ad46f1c7b1c8d34457cbf742c7ecdf
김의진 <ejkim@gim-uijin-ui-MacBookAir.local>
2024-11-01 15:58:08 +0900

    토큰 검증 수정

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -15,11 +15,12 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     req.user = decoded;
-    console.log('검증 성공:', decoded);
     next();
   } catch (error) {
-    console.error('토큰 검증 오류:', error);
-    return res.status(401).json({ message: '유효하지 않은 토큰입니다.' });
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: '토큰이 만료되었습니다.' });
+    }
+    return res.status(403).json({ message: '유효하지 않은 토큰입니다.' });
   }
 };
 
