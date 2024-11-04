df7964542e3b10a865458a8a44980a7f21b31c2e
김예원 <yaewon4643@naver.com>
2024-11-04 13:59:30 +0900

    feat: 구매 / 취소 내역 조회

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts, getPopularProducts, getNewProducts, getProduct } = require('../controllers/productController');
+const { getAllProducts, getPopularProducts, getNewProducts, getProduct, getProductsByIds } = require('../controllers/productController');
 
 // 모든 상품 조회
 router.get('/', getAllProducts);
 router.get('/popular-products', getPopularProducts);
 router.get('/new-products', getNewProducts);
 router.get('/:id', getProduct);
+
+// 여러 ID에 해당하는 상품 조회 (POST)
+router.post('/getByIds', getProductsByIds);
 
 module.exports = router;
