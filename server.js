8772f9f682731f8d9fcc92d314514ff6a2a324aa
Jugwang <hjjk0721@gmail.com>
2024-11-08 01:49:08 +0900

    fix: no credential

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use(
   cors({
     origin: ['https://moongchee.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
   })
 );
 app.use(bodyParser.json());
